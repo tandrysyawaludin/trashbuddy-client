5faa17c0be7f9084831574966c488a83059a80d0
tandrysyawaludin <syawaludintandry@yahoo.com>
2018-12-03 00:22:08 +0700

    remove dotenv

--- src/index.js
+++ src/index.js
@@ -7,11 +7,6 @@ import WebFont from 'webfontloader'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './css/index.css'
 
-// DOTENV for load variable inside .env file
-// Every single variable have to prefix 'REACT_APP_' 
-require('dotenv').config()
-// End DOTENV
-
 WebFont.load({
   google: {
     families: ['Open Sans', 'sans-serif']
